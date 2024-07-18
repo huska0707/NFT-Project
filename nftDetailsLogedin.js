e9751c50bd1c8bf7dad6044a4aa571b19bcb6071
huska0707 <hussaink001haled@gmail.com>
2024-07-18 10:10:38 -0500

    add functions in nft details logedin page

---
+++ nftDetailsLogedin.js
@@ -0,0 +1,10 @@
+let closeIcon = document.getElementById("fa-close");
+function closeform() {
+  let overlayElem = document.getElementById("overlayOne");
+  overlayElem.style.display = "none";
+}
+
+function closeform2() {
+  let overlayElem2 = document.getElementById("overlayTwo");
+  overlayElem2.style.display = "none";
+}
