3eec7490bf86b4fb4e0cc95c52f26ce03c956317
huska0707 <hussaink001haled@gmail.com>
2024-07-19 08:12:30 -0500

    add functios in nft details logedin page

--- nftDetailsLogedin.js
+++ nftDetailsLogedin.js
@@ -8,3 +8,24 @@ function closeform2() {
   let overlayElem2 = document.getElementById("overlayTwo");
   overlayElem2.style.display = "none";
 }
+
+let priceBtnTwo = document.getElementById("priceBtnTwo");
+priceBtnTwo.addEventListener("click", showForm2);
+
+function showForm2() {
+  let overlayElem2 = document.getElementById("overlayTwo");
+  overlayElem2.style.display = "block";
+}
+
+let displayBtn = document.getElementById("displayBtn");
+displayBtn.addEventListener("click", showHidden);
+
+function showHidden() {
+  let sliderElem = document.getElementById("slider");
+  sliderElem.classList.toggle("btn");
+  console.log("showHidden");
+  sliderElem.style.overflow = "auto";
+  sliderElem.style.justifyContent = "space-evenly";
+  sliderElem.style.margin = "16rem 100rem";
+  sliderElem.style.flexWrap = "wrap";
+}
